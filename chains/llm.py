025dad91507243acb55b054f4b284365b37887ec
niconeven <nneven@usc.edu>
2023-05-11 22:10:33 -0700

    Update llm.py

--- chains/llm.py
+++ chains/llm.py
@@ -3,17 +3,40 @@ from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import (
     ChatPromptTemplate,
+    SystemMessagePromptTemplate,
+    AIMessagePromptTemplate,
     HumanMessagePromptTemplate,
 )
 
 class LLMChain(LangchainLLMChain):
-    def __init__(self, llm: ChatOpenAI = ChatOpenAI()):
-        human_message_prompt = HumanMessagePromptTemplate(
-            prompt = PromptTemplate(
-                template="{message}",
-                input_variables=["message"],
+    def __init__(
+        self,
+        llm: ChatOpenAI = ChatOpenAI(),
+        chat_prompt: ChatPromptTemplate = None,
+    ):
+        if chat_prompt is None:
+            human_message_prompt_template = PromptTemplate(
+                template="{human_message}",
+                input_variables=["human_message"],
             )
-        )
+            human_message_prompt = HumanMessagePromptTemplate(prompt=human_message_prompt_template)
 
-        prompt = ChatPromptTemplate.from_messages([human_message_prompt])
-        super().__init__(llm=llm, prompt=prompt)
+            system_message_prompt_template = PromptTemplate(
+                template="{system_message}",
+                input_variables=["system_message"],
+            )
+            system_message_prompt = SystemMessagePromptTemplate(prompt=system_message_prompt_template)
+
+            ai_message_prompt_template = PromptTemplate(
+                template="{ai_message}",
+                input_variables=["ai_message"],
+            )
+            ai_message_prompt = AIMessagePromptTemplate(prompt=ai_message_prompt_template)
+
+            chat_prompt = ChatPromptTemplate.from_messages([
+                human_message_prompt,
+                system_message_prompt,
+                ai_message_prompt,
+            ])
+
+        super().__init__(llm=llm, prompt=chat_prompt)
