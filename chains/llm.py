a6ecc165d89666e56bf5de2f05a962b17b8ba221
niconeven <nneven@usc.edu>
2023-05-14 03:50:49 -0700

    update llm.py docstring

--- chains/llm.py
+++ chains/llm.py
@@ -1,42 +1,3 @@
-"""
-Brief summary of the class's purpose.
-
-Detailed explanation of the class's behavior, its role within the larger application, and how it interacts with other classes or data.
-
-Parameters
-----------
-param1 : type
-    Description of param1, including any constraints or required formats.
-param2 : type
-    Description of param2, including any constraints or required formats.
-...
-
-Attributes
-----------
-attr1 : type
-    Description of attr1, including any default values.
-attr2 : type
-    Description of attr2, including any default values.
-...
-
-Methods
--------
-method1
-    Brief description of method1.
-method2
-    Brief description of method2.
-...
-
-Example
--------
->>> ClassName(param1, param2)
-Expected result
-
-Notes
------
-Any additional notes on the class's usage within the broader application.
-"""
-
 from langchain.chains import LLMChain as LangchainLLMChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
@@ -44,6 +5,47 @@ from prompts.human_message import default_human_message_prompt
 
 
 class LLMChain(LangchainLLMChain):
+    """
+    This class extends LangChain's LLMChain for querying an LLM object using a prompt template and input key values.
+
+    LLMChain formats the prompt template with the input key values (and memory key values, if available),
+    passes the formatted string to LLM, and returns the LLM output.
+
+    Args:
+        llm (ChatOpenAI, optional): A ChatOpenAI instance for language model interaction.
+        chat_prompt (ChatPromptTemplate, optional): A ChatPromptTemplate instance for generating prompts.
+
+    Attributes:
+        llm (ChatOpenAI): The ChatOpenAI instance to use for generating text.
+        prompt (ChatPromptTemplate): The ChatPromptTemplate instance to format with input key values.
+
+     Methods:
+        __call__(input: dict) -> dict:
+            Formats the prompt using the input and passes it to the LLM.
+
+        run(input: dict) -> dict:
+            Similar to __call__, formats the prompt using the input and passes it to the LLM.
+
+        apply(input_list: list[dict]) -> list[dict]:
+            Formats the prompt using each dict in input_list and passes them to the LLM.
+
+        generate(input_list: list[dict]) -> LLMResult:
+            Similar to apply, but returns an LLMResult instead of a list of strings.
+
+        predict(**kwargs) -> str:
+            Formats the prompt using the kwargs and passes it to the LLM.
+
+        predict_and_parse(**kwargs) -> Any:
+            Similar to predict, but also applies the output parser on the LLM output.
+
+        apply_and_parse(input_list: list[dict]) -> list[Any]:
+            Similar to apply, but also applies the output parser on each LLM output.
+
+        from_string(llm: OpenAI, template: str) -> LLMChain:
+            Constructs an LLMChain from a string template.
+
+    """
+
     def __init__(
         self,
         llm: ChatOpenAI = ChatOpenAI(),
