c1ea5552b06108b22354ce019e2fc0b6d64ff44f
Brendan <b.k.morrison2@gmail.com>
2023-05-14 18:13:27 -0700

    logging

--- chains/qa.py
+++ chains/qa.py
@@ -27,13 +27,17 @@ from langchain.chains.qa_with_sources.loading import load_qa_with_sources_chain
 from langchain import OpenAI
 from .llm import LLMChain
 from langchain.chains.conversational_retrieval.prompts import CONDENSE_QUESTION_PROMPT
+from utils import logging
 
 
 class QAChain(ConversationalRetrievalChain):
-    def __init__(self, retriever, llm: OpenAI = OpenAI(verbose=True, temperature=0)):
-
+    def __init__(
+        self, 
+        retriever, 
+        llm: OpenAI = OpenAI(verbose=True, temperature=0)
+    ):
         question_generator = LLMChain(llm=llm, chat_prompt=CONDENSE_QUESTION_PROMPT)
-
         combine_docs_chain = load_qa_with_sources_chain(llm, chain_type="map_reduce")
 
         super().__init__(combine_docs_chain=combine_docs_chain, question_generator=question_generator, retriever=retriever)
+        logging.info(f"Initialized QAChain.")
