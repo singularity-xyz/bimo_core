026c216600fb4c0fb24ce3a2d25c570d8ab3f561
Brendan <b.k.morrison2@gmail.com>
2023-05-16 11:11:22 -0700

    make submodule

--- src/utils/config.py
+++ src/utils/config.py
@@ -17,3 +17,5 @@ load_dotenv()
 
 # Set OPENAI_API_KEY environment variable
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+os.environ["MONGO_HOST"] = os.getenv("MONGO_HOST")
+os.environ["MONGO_PORT"] = os.getenv("MONGO_PORT")
