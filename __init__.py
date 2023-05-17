674220e4560fa4201f126c216f4f1a4cf5c70c3c
Brendan <b.k.morrison2@gmail.com>
2023-05-16 20:19:44 -0700

    update convenience imports

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
 from momoai_core.src import chains
 from momoai_core.src import managers
 from momoai_core.src import tools
+
+from momoai_core.src.chains import LLMChain, CRChain, QAChain
+from momoai_core.src.managers import ChainManager, DocumentManager
