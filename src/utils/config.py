6a9515aa2dee777871b7ffbb94f593a9fc2892ff
niconeven <nneven@usc.edu>
2023-05-16 22:51:05 -0700

    add google.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,6 +1,5 @@
 import logging
 
-# Setup logging
 logging.basicConfig(
     level=logging.INFO,
     format="[%(levelname)s] %(message)s",
---
+++ src/utils/google.py
@@ -0,0 +1,55 @@
+from google.cloud import storage
+
+
+class GoogleCloudManager:
+    def __init__(self, bucket_name):
+        self.client = storage.Client()
+        self.bucket_name = bucket_name
+        self.bucket = self.client.get_bucket(self.bucket_name)
+
+    def _get_blob(self, blob_name):
+        """Creates a blob object."""
+        blob = self.bucket.blob(blob_name)
+        return blob
+
+    def upload_blob(self, source_file_name, destination_blob_name):
+        """Uploads a file to the bucket."""
+        blob = self._get_blob(destination_blob_name)
+        blob.upload_from_filename(source_file_name)
+
+    def download_blob(self, blob_name, destination_file_name):
+        """Downloads a blob from the bucket."""
+        blob = self._get_blob(blob_name)
+        blob.download_to_filename(destination_file_name)
+
+    def list_blobs(self, prefix=None):
+        """Lists all the blobs in the bucket."""
+        blobs = self.bucket.list_blobs(prefix=prefix)
+        return blobs
+
+    def delete_blob(self, blob_name):
+        """Deletes a blob from the bucket."""
+        blob = self._get_blob(blob_name)
+        blob.delete()
+
+    def generate_signed_url(self, blob_name, expiration=300):
+        """Generates a signed URL for a blob."""
+        blob = self._get_blob(blob_name)
+        url = blob.generate_signed_url(expiration=expiration)
+        return url
+
+    def get_blob_metadata(self, blob_name):
+        """Get metadata of a blob."""
+        blob = self._get_blob(blob_name)
+        return blob.metadata
+
+    def update_blob_metadata(self, blob_name, metadata):
+        """Update metadata of a blob."""
+        blob = self._get_blob(blob_name)
+        blob.metadata = metadata
+        blob.patch()
+
+    def get_blob_info(self, blob_name):
+        """Get all info of a blob."""
+        blob = self._get_blob(blob_name)
+        return blob._properties
