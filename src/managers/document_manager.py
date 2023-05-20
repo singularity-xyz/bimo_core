2fe85c6173c4ba601e311fc6b95b03243f696e46
niconeven <nneven@usc.edu>
2023-05-20 02:57:36 -0700

    Update document_manager.py

--- src/managers/document_manager.py
+++ src/managers/document_manager.py
@@ -21,7 +21,25 @@ class DocumentMetadata:
 class DocumentManager:
     def __init__(self, gcs_client: GCSClient=None, vector_store: DeepLake=None):
         self.gcs_client = gcs_client or GCSClient(bucket_name="momo-ai")
-        self.vector_store = vector_store or DeepLake(dataset_path="deeplake_dataset", embedding_function=OpenAIEmbeddings())
+        self.vector_store = vector_store or DeepLake(dataset_path="deeplake", embedding_function=OpenAIEmbeddings())
+
+    # somewhere in here we need to add the metadata to mongo db
+    def upload_document(self, document_metadata: DocumentMetadata, file_content: BytesIO) -> None:
+        destination_blob_name = self._generate_blob_name(document_metadata)
+        self.gcs_client.upload_blob(file_content, destination_blob_name)
+        # After uploading the blob, reset the position of the file content
+        file_content.seek(0)
+        self._upload_embeddings(document_metadata, file_content)
+
+    def get_document(self, document_metadata: DocumentMetadata) -> None:
+        blob_name = self._generate_blob_name(document_metadata)
+        return self.gcs_client.download_blob(blob_name)
+
+    def get_vector_store(self) -> DeepLake:
+        return self.vector_store
+
+    def get_retriever(self) -> BaseRetriever:
+        return self.vector_store.as_retriever()
 
     def _generate_blob_name(self, document_metadata: DocumentMetadata) -> str:
         return f"{document_metadata.user_id}/{document_metadata.id}"
@@ -39,20 +57,3 @@ class DocumentManager:
             texts = text_splitter.split_documents(documents)
 
             self.vector_store.add_documents(texts)
-
-    def upload_document(self, document_metadata: DocumentMetadata, file_content: BytesIO) -> None:
-        destination_blob_name = self._generate_blob_name(document_metadata)
-        self.gcs_client.upload_blob(file_content, destination_blob_name)
-        # After uploading the blob, reset the position of the file content
-        file_content.seek(0)
-        self._upload_embeddings(document_metadata, file_content)
-
-    def get_document(self, document_metadata: DocumentMetadata) -> None:
-        blob_name = self._generate_blob_name(document_metadata)
-        return self.gcs_client.download_blob(blob_name)
-
-    def get_vector_store(self) -> DeepLake:
-        return self.vector_store
-
-    def get_retriever(self) -> BaseRetriever:
-        return self.vector_store.as_retriever()
